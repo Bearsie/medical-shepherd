ac483655b773b8339d4875d63c24e5771dfea620
Grzegorz Roczniak <bigos234@o2.pl>
2019-06-18 00:05:57 +0200

    Add smart select for Risk Factors

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -24,28 +24,31 @@ import { mergeStyles } from '@uifabric/merge-styles';
       <Col className={mergeStyles({ textAlign: '-webkit-center'})}>
 
 
-  <div className={mergeStyles({ width: '250px', height: '250px' })}>
-  <div className="item-title">Years</div>
+  <div className={mergeStyles({ width: '250px', height: '150px' })}>
+  <div className="item-title">Age</div>
       <div className="block block-strong">
        <div className="range-slider range-slider-init" data-label="true" data-value-left="18" data-value-right="122" >
   <input type="range" min="18" max="122" step="1" value="10" />
        </div>
       </div>
+      </div>
       
-
+      <div className={mergeStyles({ width: '100px', height: '150px' })}>
       <div className="item-content item-input"></div>
         <div className="item-inner">
         <div className="item-title item-label">Sex</div>
         <div className="block block-strong">
-          <div className="item-input-wrap">
+          <div className="item-input-wrap input-dropdown-wrap">
             <select name="gender">
-              <option value="male" selected>Male</option>
+              <option value="male" >Male</option>
               <option value="female">Female</option>
             </select>
           </div>
           </div>
       </div>
+      </div>
 
+      <div className={mergeStyles({ width: '250px', height: '250px' })}>
       <div className="block-title">Weight(kg)</div>
       <div className="block block-strong">
        <div className="range-slider range-slider-init" data-label="true" data-value-left="30" data-value-right="200" >
@@ -60,55 +63,218 @@ import { mergeStyles } from '@uifabric/merge-styles';
        </div>
       </div>
 
-<div className="list">
-  <ul>
-    <li>
-      <label className="item-checkbox item-content">
-        <input type="checkbox" name="demo-checkbox" value="High Cholesterol"/>
-        <i className="icon icon-checkbox"></i>
+        <div className="item-content item-input"></div>
         <div className="item-inner">
-          <div className="item-title">High Cholesterol</div>
-        </div>
-      </label>
-    </li>
-    <li>
-      <label className="item-checkbox item-content">
-        <input type="checkbox" name="demo-checkbox" value="Hypertension"/>
-        <i className="icon icon-checkbox"></i>
-        <div className="item-inner">
-          <div className="item-title">Hypertension</div>
-        </div>
-      </label>
-    </li>
-    <li>
-      <label className="item-checkbox item-content">
-        <input type="checkbox" name="demo-checkbox" value="Diabetes"/>
-        <i className="icon icon-checkbox"></i>
-        <div className="item-inner">
-          <div className="item-title">Diabetes</div>
-        </div>
-      </label>
-    </li>
-    <li>
-      <label className="item-checkbox item-content">
-        <input type="checkbox" name="demo-checkbox" value="Tumor"/>
-        <i className="icon icon-checkbox"></i>
-        <div className="item-inner">
-          <div className="item-title">Tumor</div>
-        </div>
-      </label>
-    </li>
-  </ul>
-</div>
+        <div className="item-title item-label">Place of Residence</div>
+        <div className="block block-strong">
+          <div className="item-input-wrap input-dropdown-wrap">
+            <select placeholder="Please choose...">
+              <option value="United States or Canada">United States or Canada</option>
+              <option value="Latin or South America">Latin or South America</option>
+              <option value="Europe">Europe</option>
+              <option value="Northern Africa">Northern Africa</option>
+              <option value="Central Africa">Central Africa</option>
+              <option value="Southern Africa">Southern Africa</option>
+              <option value="Australia and Oceania">Australia and Oceania</option>
+              <option value="Russia, Kazakhstan or Mongolia">Russia, Kazakhstan or Mongolia</option>
+              <option value="Southwestern Asia">Southwestern Asia</option>
 
-          <Button
+            </select>
+          
+          </div>
+          </div>
+          </div>
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">High Cholesterol</div>
+      </div>
+  </a>
+</div>
+</li>
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">High Cholesterol</div>
+      </div>
+  </a>
+</div>
+</li> 
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">High Cholesterol</div>
+      </div>
+  </a>
+</div>
+</li> 
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">Diabetes</div>
+      </div>
+  </a>
+</div>
+</li> 
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">Hypertension</div>
+      </div>
+  </a>
+</div>
+</li> 
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">Smoking</div>
+      </div>
+  </a>
+</div>
+</li> 
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">Frequent alcohol consumption</div>
+      </div>
+  </a>
+</div>
+</li> 
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">Coronary disease</div>
+      </div>
+  </a>
+</div>
+</li> 
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">Contact lenses</div>
+      </div>
+  </a>
+</div>
+</li> 
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">Chronic obstructive pulmonary disease</div>
+      </div>
+  </a>
+</div>
+</li> 
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">Asthma</div>
+      </div>
+  </a>
+</div>
+</li> 
+
+<li>
+<div className="item-input-wrap input-dropdown-wrap">
+  <a className="smart-select smart-select-init">
+    <select name="High Cholesterol">
+      <option value="unknown" selected>Unknown</option>
+      <option value="yes">Yes</option>
+      <option value="pear">No</option>
+    </select>
+      <div className="item-inner">
+        <div className="item-title">Atherosclerosis</div>
+      </div>
+  </a>
+</div>
+</li> 
+  
+  <Button
             fill
             onClick={Save}
             className={mergeStyles({ width: '50%'})}
           >
             Apply
           </Button>
-          </div>         
+
+      </div>
+      
       </Col>
       </Page>
 
